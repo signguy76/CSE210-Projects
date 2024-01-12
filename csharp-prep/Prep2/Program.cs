6fa7c4e0b2aa54e3881c60711c77f75de88f41fd
signguy76 <coates.neal@gmail.com>
2024-01-11 17:00:39 -0800

    String values

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        string g;
         Console.Write("What is your grade percentage? ");
         string yourGrade = Console.ReadLine();
         int yourGradeNumber = int.Parse (yourGrade);
@@ -13,35 +12,30 @@ class Program
         Console.Write($"At {yourGradeNumber} %, ");
         if (yourGradeNumber > 90 || yourGradeNumber == 90)
         {
-
-            g = "A";
-            Console.WriteLine ($"your grade is an {g}.");
+            string letter = "A";           
+            Console.WriteLine ($"your grade is an {letter}.");
         }
         else if (yourGradeNumber < 90 && yourGradeNumber > 80 || yourGradeNumber == 80)
         {
-            g = "B";
-            Console.WriteLine ($"your grade is a {g}.");
+            string letter = "B";
+            
+            Console.WriteLine ($"your grade is a {letter}.");
         }else if (yourGradeNumber < 80 && yourGradeNumber > 70 || yourGradeNumber == 70)
         {
-            Console.WriteLine ("your grade is a C.");
+            string letter = "C";
+            Console.WriteLine ($"your grade is a {letter}.");
         }
         else if (yourGradeNumber < 70 && yourGradeNumber > 60 || yourGradeNumber == 60)
         {
-            Console.WriteLine ("your grade is a D.");
+            string letter = "D";
+            Console.WriteLine ($"your grade is a {letter}.");
         }
         else if (yourGradeNumber < 60 )
         {
-            Console.WriteLine ("your grade is and F. Study harder.");
+            string letter = "F";
+            Console.WriteLine ($"your grade is an {letter}.");
         }
-        if ( yourGradeNumber >70 || yourGradeNumber == 70)
-        {
-            Console.WriteLine ("Congratulations, you have passed the course.");
-        }
-        else if (yourGradeNumber < 70)
-        {
-            Console.WriteLine ("A grade of 70% or better is required to pass this course. Please re-enroll to take the class again.");
-
-        }
-
+        
+        
     }
 }
