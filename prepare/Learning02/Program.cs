7df83d439b74003da5e0d3c51b13f5f545c89905
signguy76 <coates.neal@gmail.com>
2024-01-15 13:05:03 -0800

    Initial Job

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        public class Job
+        {
+            public string _company;
+            public string _jobTitle;
+            public int _startYear;
+            public int _endYear;
+
+            public void Display()
+            {
+                Console.WriteLine($"{_company}, {_jobTitle}, {_startYear} {_endYear} ");
+            }
+        }
     }
 }
