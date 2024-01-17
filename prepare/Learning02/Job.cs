e419fc1898401cc6de0b4fd2c0c5b4f72ed30f8c
signguy76 <coates.neal@gmail.com>
2024-01-17 14:14:47 -0800

    Jobs

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -3,11 +3,10 @@ public class Job
     public string _company;
     public string _jobTitle;
     public int _startYear;
-    public int _endYear;
-    public int _oneMore;
+    public int _endYear;    
 
     public void Display()
     {
-        Console.WriteLine($"{_company}, {_jobTitle}, {_startYear} {_endYear} ");
+        Console.WriteLine($" {_jobTitle} ({_company}) {_startYear}-{_endYear} ");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,11 +5,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
-        Job job = new();
-        job._jobTitle = "Engineer";
-        job._startYear = 2010;
-        job._endYear = 2020;
-        job.Display();
+        
+        Job job1 = new();
+        job1._company = "Crown Graphics";
+        job1._jobTitle = "Installation Manager";
+        job1._startYear = 1998;
+        job1._endYear = 2011;
+
+        Job job2 = new();
+        job2._company = "Sign Factory";
+        job2._jobTitle = "Salesperson";
+        job2._startYear = 2011;
+        job2._endYear = 2015;
+
+        Job job3 = new();
+        job3._company = "Signs Now";
+        job3._jobTitle = "Sales Manager";
+        job3._startYear = 2015;
+        job3._endYear = 2023;        
+
+        Resume myResume = new Resume();
+        myResume._name = "Johnny Williams";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
     }
 }
